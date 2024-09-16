0b0d7e4baeddca3c8985721e8ab567dfc7b80edc
ice909 <tonimayloneya@gmail.com>
2024-09-16 15:07:10 +0800

    feat: Introducing ant-design
    引入ant-design组件库，并完成网站基本布局

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,17 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
   {
     path: '',
+    pathMatch: 'full',
+    redirectTo: '/home'
+  },
+  {
+    path: 'home',
     component: HomeComponent,
-    title: '首页'
+    data: {
+      title: 'Home'
+    }
   }
 ];
